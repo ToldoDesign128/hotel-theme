c7f67e69cc52f665b8f949fa107badaa61054016
Federico <fede.toldo96@gmail.com>
2023-12-13 17:25:12 +0100

    gsap

---
+++ assets/js/gsap.js
@@ -0,0 +1,14 @@
+// wait until DOM is ready
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    console.log("DOM loaded");
+
+    //wait until images, links, fonts, stylesheets, and js is loaded
+    window.addEventListener("load", function (e) {
+
+        //custom GSAP code goes here
+        
+
+    }, false);
+
+});
--- functions.php
+++ functions.php
@@ -102,6 +102,18 @@ if (function_exists('acf_add_options_page')) {
 	));
 }
 
+//GSAP
+function theme_gsap_script(){
+    // The core GSAP library
+    wp_enqueue_script( 'gsap-js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/gsap.min.js', array(), false, true );
+    // ScrollTrigger - with gsap.js passed as a dependency
+    wp_enqueue_script( 'gsap-st', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.3/ScrollTrigger.min.js', array('gsap-js'), false, true );
+    // Your animation code file - with gsap.js passed as a dependency
+    wp_enqueue_script( 'gsap-js2', get_template_directory_uri() . '/assets/js/gsap.js', array('gsap-js'), false, true );
+}
+
+add_action( 'wp_enqueue_scripts', 'theme_gsap_script' );
+
 // Add style and script
 function add_theme_scripts()
 {

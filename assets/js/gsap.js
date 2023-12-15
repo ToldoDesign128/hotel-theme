d6bb7305c66eae85d680d1570b5d80126d53a03d
Federico <fede.toldo96@gmail.com>
2023-12-15 10:33:28 +0100

    Apply gsap on hero and fix mobile

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -1,13 +1,16 @@
 // wait until DOM is ready
 document.addEventListener("DOMContentLoaded", function (event) {
 
-    console.log("DOM loaded");
-
     //wait until images, links, fonts, stylesheets, and js is loaded
     window.addEventListener("load", function (e) {
 
-        //custom GSAP code goes here
-        
+        let tl = gsap.timeline()
+
+        //Hero Animation
+        tl
+            .to("#hero > .textContainer > .heroAnimation1", { y: 0, duration: .6, opacity: 1 })
+            .to("#hero > .textContainer > .heroAnimation2", { y: 0, duration: .6, opacity: 1 })
+            .to("#hero > .textContainer > .heroAnimation3", { y: 0, duration: .6, opacity: 1 },"<");
 
     }, false);
 
--- front-page.php
+++ front-page.php
@@ -1,18 +1,18 @@
 <?php get_header();
 $page_id = get_queried_object_id(); ?>
 <!-- Hero -->
-<section class="w-full h-screen flex flex-row">
+<section id="hero" class="w-full lg:h-screen h-auto flex lg:flex-row flex-wrap overflow-x-hidden">
     <?php if (have_rows('section_1')) : ?>
         <?php while (have_rows('section_1')) : the_row();
 
             $hero_text = get_sub_field('testo_hero');
         ?>
-            <div class="lg:w-1/2 h-full w-full">
+            <div class="lg:w-1/2 lg:h-full h-[60vh] w-full">
                 <?php $video = get_sub_field('video_hero'); ?>
                 <video class="h-full object-cover" src="<?php echo esc_attr($video); ?>" autoplay muted loop></video>
             </div>
             <!-- Hero text -->
-            <div class="lg:w-1/2 h-full w-full flex flex-col justify-center pl-12">
+            <div class="textContainer lg:w-1/2 lg:h-full h-auto w-full flex flex-col justify-center lg:pl-24 px-6 py-24">
 
                 <?php if (have_rows('testo_hero')) : ?>
                     <?php while (have_rows('testo_hero')) : the_row();
@@ -26,7 +26,7 @@ $page_id = get_queried_object_id(); ?>
                         $hero_pulsante_target = $hero_pulsante['target'] ? $hero_pulsante['target'] : '_self';
                     ?>
 
-                        <div class="py-4">
+                        <div class="heroAnimation1 py-4">
                             <h1 class="font-serif 2xl:text-8xl lg:text-7xl text-6xl text-stone-200">
                                 <?php echo esc_html($hero_title); ?>
                             </h1>
@@ -34,10 +34,10 @@ $page_id = get_queried_object_id(); ?>
                                 <?php echo esc_html($hero_subtitle); ?>
                             </h2>
                         </div>
-                        <div class="lg:w-8/12 w-full lg:text-xl text-lg py-4 text-stone-400">
+                        <div class="heroAnimation2 lg:w-8/12 w-full lg:text-xl text-lg py-4 text-stone-400">
                             <?php echo the_sub_field('testo_hero_paragrafo'); ?>
                         </div>
-                        <a class="w-fit uppercase tracking-wider text-stone-100 lg:hover:text-stone-900 border bg-transparent lg:hover:bg-stone-100 border-stone-100 py-2 px-5 mt-4 transition-all duration-300" href="<?php echo esc_url($hero_pulsante_url); ?>" target="<?php echo esc_attr($hero_pulsante_target); ?>"><?php echo esc_html($hero_pulsante_text); ?></a>
+                        <a class="heroAnimation3 w-fit uppercase tracking-wider text-stone-100 lg:hover:text-stone-900 border bg-transparent lg:hover:bg-stone-100 border-stone-100 py-2 px-5 mt-4 transition-all duration-300" href="<?php echo esc_url($hero_pulsante_url); ?>" target="<?php echo esc_attr($hero_pulsante_target); ?>"><?php echo esc_html($hero_pulsante_text); ?></a>
 
                     <?php
                     endwhile; ?>
